efe3a0aba55571b87f516600d736af4f7b286298
TOSPUR\63081 <huochangsheng@tospur.com>
2019-06-05 14:59:45 +0800

    添加 Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,19 +1,22 @@
 <?php
 
-namespace App;
-
+namespace App\Models;
+// 消息通知相关功能引用
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+// 是授权相关功能的引用
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
+     * 添加了 fillable 在过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新
      */
     protected $fillable = [
         'name', 'email', 'password',
@@ -21,7 +24,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     * 当我们需要对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏，则可使用 hidden 属性
      * @var array
      */
     protected $hidden = [
