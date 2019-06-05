be844b10be6e968a7acc29043d827facfbe1b81c
huocs <huo8008@126.com>
2019-06-05 17:13:29 +0800

    显示用户信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,5 +3,16 @@
 @section('title', $user->name)
 
 @section('content')
-    {{ $user->name }} - {{ $user->email }}
+    <div class="row">
+      <div class="offset-md-2 col-md-8">
+        <div class="col-md-12">
+          <div class="offset-md-2 col-md-8">
+            <section class="user_info">
+              {{--可以通过给 @include 方法传参，将用户数据以关联数组的形式传送到 _user_info 局部视图上--}}
+              @include('shared._user_info', ['user' => $user])
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
 @stop
