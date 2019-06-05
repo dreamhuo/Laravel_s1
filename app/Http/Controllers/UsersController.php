624f8d2b2e0315f197a601c46b61096c9c8ee523
huocs <huo8008@126.com>
2019-06-05 16:54:18 +0800

    模板变量渲染

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+    public function show(User $user)
+    {
+        // 将用户对象 $user 通过 compact 方法转化为一个关联数组
+        // 作为第二个参数传递给 view 方法，将数据与视图进行绑定
+        // show 方法添加完成之后，我们便能在视图中使用 user 变量来访问通过
+        return view('users.show', compact('user'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
+
+// Laravel 提供了 resource 方法来定义用户资源路由
+// resource 方法将遵从 RESTful 架构为用户资源生成路由。
+// 该方法接收两个参数，第一个参数为资源名称，第二个参数为控制器名称
+Route::resource('users', 'UsersController');
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
