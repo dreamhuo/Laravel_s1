5c8d18aa6405b77917100467389e025047dc66dc
huocs <huo8008@126.com>
2019-06-10 15:02:06 +0800

    用户登录

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('signup', 'UsersController@create')->name('signup');
 // 该方法接收两个参数，第一个参数为资源名称，第二个参数为控制器名称
 Route::resource('users', 'UsersController');
 // Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
