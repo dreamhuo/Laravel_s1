140727483a732c27fe77bf78e148ed603333877a
huocs <huo8008@126.com>
2019-06-11 13:11:21 +0800

    添加权限验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,20 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    // __construct 是 PHP 的构造器方法，当一个类对象被创建之前该方法将会被调用。
+    public function __construct()
+    {
+        // 我们在 __construct 方法中调用了 middleware 方法，该方法接收两个参数
+        // 第一个为中间件的名称，第二个为要进行过滤的动作。
+        // 通过 except 方法来设定 指定动作 不使用 Auth 中间件进行过滤
+        // 除了此处指定的动作 'show', 'create', 'store' 以外，所有其他动作都必须登录用户才能访问
+        // 相反的还有 only 白名单方法，将只过滤指定动作
+        // 提倡在控制器 Auth 中间件使用中，首选 except 方法，这样的话，当你新增一个控制器方法时，默认是安全的
+        // Auth 中间件在过滤指定动作时，若该用户未通过身份验证（未登录用户），默认将会被重定向到 /login 登录页面
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+    }
     public function create()
     {
         return view('users.create');
