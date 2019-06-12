a1409c179af2d5da45ca904e10c5b857e5c3682e
huocs <huo8008@126.com>
2019-06-12 11:11:35 +0800

    显示统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,6 +11,9 @@
               {{--可以通过给 @include 方法传参，将用户数据以关联数组的形式传送到 _user_info 局部视图上--}}
               @include('shared._user_info', ['user' => $user])
             </section>
+            <section class="stats mt-2">
+              @include('shared._stats', ['user' => $user])
+            </section>
             <section class="status">
               @if ($statuses->count() > 0)
                 <ul class="list-unstyled">
