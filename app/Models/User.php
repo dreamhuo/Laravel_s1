5dcd576568c4d9bfef4c73293448f2ceaac6ad2d
huocs <huo8008@126.com>
2019-06-12 11:01:30 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -77,4 +77,53 @@ class User extends Authenticatable
         return $this->statuses()
                     ->orderBy('created_at', 'desc');
     }
+
+    // 我们可以通过 followers 来获取粉丝关系列表
+    public function followers()
+    {
+        // 在 Laravel 中我们使用 belongsToMany 来关联模型之间的多对多关系
+        // 一个关注者可以有多个粉丝， 也可以关注多个粉丝
+        // return $this->belongsToMany(User::Class);
+        // 在 Laravel 中会默认将两个关联模型的名称进行合并，并按照字母排序，因此我们生成的关联关系表名称会是 user_user。我们也可以自定义生成的名称，把关联表名改为 followers
+        // return $this->belongsToMany(User::Class, 'followers');
+        // 通过传递额外参数至 belongsToMany 方法来自定义数据表里的字段名称
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 通过 followings 来获取用户关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注动作
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        // sync 方法在中间表上创建一个多对多记录
+        // sync 方法会接收两个参数
+        // 第一个参数为要进行添加的 id
+        // 第二个参数则指明是否要移除其它不包含在关联的 id 数组中的 id，true 表示移除，false 表示不移除，默认值为 true
+        // 由于我们在关注一个新用户的时候，仍然要保持之前已关注用户的关注关系，因此不能对其进行移除，所以在这里我们选用 false
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注动作
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        // detach 方法在中间表上移除一个记录
+        $this->followings()->detach($user_ids);
+    }
+
+    // 判断当前登录的用户 A 是否关注了用户 B，只需判断用户 B 是否包含在用户 A 的关注人列表上即可。
+    public function isFollowing($user_id)
+    {
+        // 这里我们将用到 contains 方法来做判断
+        return $this->followings->contains($user_id);
+    }
 }
