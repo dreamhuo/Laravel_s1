50a07ac1b9f22880e8b59d405eb01f5bbb5be8b2
huocs <huo8008@126.com>
2019-06-12 10:23:06 +0800

    删除按钮

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
 use Auth;
 
 class StatusesController extends Controller
@@ -21,6 +22,18 @@ class StatusesController extends Controller
             'content' => $request['content']
         ]);
         session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+
+    // 这里我们使用的是『隐性路由模型绑定』功能，Laravel 会自动查找并注入对应 ID 的实例对象 $status
+    public function destroy(Status $status)
+    {
+        // 做删除授权的检测，不通过会抛出 403 异常。
+        $this->authorize('destroy', $status);
+        // 调用 Eloquent 模型的 delete 方法对该微博进行删除。
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
+        // 删除成功之后，将返回到执行删除微博操作的页面上
         return redirect()->back();
     }
 }
