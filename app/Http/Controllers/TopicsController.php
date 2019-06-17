5a1cfd31074941396b68a4b6c29ec00450222a44
huocs <huo8008@126.com>
2019-06-17 15:25:02 +0800

    上传图片功能集成

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Auth;
+
+use App\Handlers\ImageUploadHandler;
+
 class TopicsController extends Controller
 {
     public function __construct()
@@ -52,5 +55,28 @@ class TopicsController extends Controller
         // $topic->save() 保存到数据库中
         $topic->save();
         return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+    }
+
+    // 上传图片接口
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success'   => false,
+            'msg'       => '上传失败!',
+            'file_path' => ''
+        ];
+        // 判断是否有上传文件，并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg']       = "上传成功!";
+                $data['success']   = true;
+            }
+        }
+        return $data;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,7 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'st
 // 分类列表话题路由
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
+// 上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+
