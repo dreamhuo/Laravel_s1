279abb6404fc074f282440bb992b4225b0012c1c
huocs <huo8008@126.com>
2019-06-18 11:05:15 +0800

    更新代码

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -17,9 +17,10 @@ class TopicsController extends Controller
     }
     public function index()
     {
+        $categories = Category::all();
         // 方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存。后面即使是在遍历数据时使用到这两个关联属性，数据已经被预加载并缓存，因此不会再产生多余的 SQL 查询
         $topics = Topic::with('user', 'category')->paginate(30);
-        return view('topics.index', compact('topics'));
+        return view('topics.index', compact('topics', 'categories'));
     }
 
     // 展示页面
