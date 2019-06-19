1219016b9e5a694b07df54a2a89cc49a6538f8fa
huocs <huo8008@126.com>
2019-06-19 13:41:04 +0800

    站内通知

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,9 +12,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Str;
 use Auth;
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
 // class User extends Authenticatable
 class User extends Authenticatable implements MustVerifyEmailContract
 {
@@ -64,17 +61,13 @@ class User extends Authenticatable implements MustVerifyEmailContract
     // 消息通知类
     public function notify($instance)
     {
-        $log = new Logger('register');
-        $log->pushHandler(new StreamHandler(storage_path('logs/laravelNotify.log'),Logger::INFO) );
         // 如果要通知的人是当前用户，就不必通知了！
         if ($this->id == Auth::id()) {
             return;
         }
         // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
         if (method_exists($instance, 'toDatabase')) {
-            $log->addInfo('notification_count');
             $this->increment('notification_count');
-            $log->addInfo('notification_count:::'.$this->notification_count);
         }
         $this->laravelNotify($instance);
     }
