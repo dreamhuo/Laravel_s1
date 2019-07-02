f6b063783e15eeda1991c85106f3b2c2ca9939b0
huocs <huo8008@126.com>
2019-07-02 14:15:48 +0800

    添加用户接口

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,12 @@ $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
     // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store') -> name('api.verificationCodes.store');
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        -> name('api.verificationCodes.store');
+    // 用户注册
+    $api->post('users', 'UsersController@store')
+        ->name('api.users.store');
+    // 测试接口
     $api->get('version', function() {
         return response('这是 version v1');
     });
