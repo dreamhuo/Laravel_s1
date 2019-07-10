06fe0f9436030d59b2a362ded4fbded384ddd3cb
huocs <huo8008@126.com>
2019-07-10 16:55:12 +0800

    消息设为已读

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -14,4 +14,17 @@ class NotificationsController extends Controller
 
         return $this->response->paginator($notifications, new NotificationTransformer());
     }
+    public function stats()
+    {
+        return $this->response->array([
+            'unread_count' => $this->user()->notification_count,
+        ]);
+    }
+    // 所有未读消息标记为已读
+    public function read()
+    {
+        // markAsRead 方法 会将用户 notification_count 设置为 0，将所有未读消息设置为已读
+        $this->user()->markAsRead();
+        return $this->response->noContent();
+    }
 }
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -20,6 +20,11 @@ class ReplyObserver
 
         // 通知话题作者有新的评论
         // notify() ，此方法接收一个通知实例做参数
+        // 如果评论的作者不是话题的作者，才需要通知
+        // if ( ! $reply->user->isAuthorOf($topic)) {
+        //     $topic->user->notify(new TopicReplied($reply));
+        // }
+        // notify 方法会将 notification_count 进行 +1。所以 $this->user()->notification_count; 就是用户未读消息数
         $reply->topic->user->notify(new TopicReplied($reply));
     }
 
