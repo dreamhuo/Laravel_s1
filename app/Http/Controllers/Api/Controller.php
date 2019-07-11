e181145aaff06fa32fddf8ce051da48b8cbd20d7
huocs <huo8008@126.com>
2019-07-11 14:01:00 +0800

    错误方法封装

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Dingo\Api\Routing\Helpers;
 use App\Http\Controllers\Controller as BaseController;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Controller extends BaseController
 {
     use Helpers;
+
+    // 任意 API 控制器中直接使用 $this->errorResponse
+    // 示例： return $this->errorResponse(403, '您还没有通过认证', 1003);
+    // {'message' : '您还没有通过认证'， 'code' :'1003', 'status_code' : '403'}
+    public function errorResponse($statusCode, $message=null, $code=0)
+    {
+        throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
