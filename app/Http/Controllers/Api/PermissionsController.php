446c2822b6b539bf09f9c57b63fb1eb3fdf10e72
huocs <huo8008@126.com>
2019-07-11 13:21:46 +0800

    更新代码

--- app/Http/Controllers/Api/PermissionsController.php
+++ app/Http/Controllers/Api/PermissionsController.php
@@ -9,8 +9,9 @@ class PermissionsController extends Controller
 {
     public function index()
     {
+        // 返回 【所有】 用户通过赋予角色所继承的权限
        $permissions = $this->user()->getAllPermissions();
-
+       // 分类数据是集合，所以我们使用 $this->response->collection 返回数据
        return $this->response->collection($permissions, new PermissionTransformer());
     }
 }
