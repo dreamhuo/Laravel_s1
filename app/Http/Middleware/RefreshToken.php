afe43ac10176a81890b107055775755c2b089181
huocs <huo8008@126.com>
2019-07-12 11:19:53 +0800

    token 刷新

--- app/Http/Middleware/RefreshToken.php
+++ app/Http/Middleware/RefreshToken.php
@@ -27,10 +27,14 @@ class RefreshToken extends BaseMiddleware
         } catch (TokenExpiredException $exception) {
           // 此处捕获到了 token 过期所抛出的 TokenExpiredException 异常，我们在这里需要做的是刷新该用户的 token 并将它添加到响应头中
             try {
-                // 刷新用户的 token
+                //首先获得过期token 接着刷新token 再接着设置token并且验证token合法性
                 $token = $this->auth->refresh();
-               // 使用一次性登录以保证此次请求的成功
-                Auth::guard('api')->onceUsingId($this->auth->manager()->getPayloadFactory()->buildClaimsCollection()->toPlainArray()['sub']);
+                $this->auth->setToken($token);
+                $request->user = $this->auth->authenticate($token);
+                // token被刷新之后，保证本次请求在controller中需要根据token调取登录用户信息能够执行成功
+                $request->headers->set('Authorization','Bearer '.$token);
+
+
             } catch (JWTException $exception) {
                // 如果捕获到此异常，即代表 refresh 也过期了，用户无法刷新令牌，需要重新登录。
                 throw new UnauthorizedHttpException('jwt-auth', $exception->getMessage());
