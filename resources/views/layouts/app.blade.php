38c480fab47a6841a04988da9d57bec3e07802a7
huocs <huo8008@126.com>
2019-07-16 10:37:45 +0800

    CSRF

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- csrf-token 标签是为了方便前端的 JavaScript 脚本获取 CSRF 令牌 -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="X-CSRF-TOKEN" content="{{ csrf_token() }}">
 
   <!-- @yield('title', 'LaraBBS') 继承此模板的页面，如果没有定制 title 区域的话，就会自动使用第二个参数 LaraBBS 作为标题前缀 -->
   <title>@yield('title', 'LaraBBS')</title>
