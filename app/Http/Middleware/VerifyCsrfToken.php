06f742e89a0fc43a36e2350ff5fdef65b1b5c710
huocs <huo8008@126.com>
2019-07-16 10:01:55 +0800

    CSRF

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Symfony\Component\HttpFoundation\Cookie;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
@@ -12,7 +13,27 @@ class VerifyCsrfToken extends Middleware
      * @var bool
      */
     protected $addHttpCookie = true;
+    /**
+     * Add the CSRF token to the response cookies.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    // 将 token 加到 cookie 里
+    protected function addCookieToResponse($request, $response)
+    {
+        $config = config('session');
 
+        $response->headers->setCookie(
+            new Cookie(
+                'dreamToken', $request->session()->token(), $this->availableAt(60 * $config['lifetime']),
+                $config['path'], $config['domain'], $config['secure'], false, false, $config['same_site'] ?? null
+            )
+        );
+
+        return $response;
+    }
     /**
      * The URIs that should be excluded from CSRF verification.
      *
